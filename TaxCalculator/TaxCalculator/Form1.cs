a7cd94d45d4556b04a79ababd56a098544192d1a
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-11-07 17:14:44 +0700

    Tax Calculator

---
+++ TaxCalculator/TaxCalculator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {            
+            InitializeComponent();
+            this.btnComputer.Click += btnComputer_Click;
+        }
+
+        void btnComputer_Click(object sender, EventArgs e)
+        {
+            String strToTalAmount = this.txtTotalAmount.Text;
+            int TotalAmount = int.Parse(strToTalAmount);
+
+            string strTaxPercentage = this.label5.Text;
+            strTaxPercentage = strTaxPercentage.Replace("%", "");
+            double taxPerCentage = double.Parse(strTaxPercentage);
+
+            double totalDue = TotalAmount * (100 + taxPerCentage) / 100;
+            this.totalDue.Text = totalDue.ToString();
+        }
+        
+    }
+}
